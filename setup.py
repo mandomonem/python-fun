4c5fc6ddbe314c9e1fb7cebefdb72d92cce33cfc
mandomonem <m.abdlmonem@gmail.com>
2022-09-10 20:46:57 +0200

    Add vcs.xml and setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#  Copyright © $Year Youssef Land. All rights reserved.
+
+from setuptools import setup
+
+setup(
+    name='python_fun',
+    version='1.0.0',
+    packages=['helper'],
+    url='https://github.com/mandomonem/python-fun',
+    license='MIT License (GitHub MIT License)',
+    author='Youssef Nasr',
+    author_email='yoyo.monem22@gmail.com',
+    description='Just for fun'
+)
