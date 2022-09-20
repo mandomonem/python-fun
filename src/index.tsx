ba71c225c1390aed4bd9a5115abf801174820023
mandomonem <m.abdlmonem@gmail.com>
2022-09-20 17:41:02 +0200

    Make a lot of changes

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+const index = ReactDOM.createRoot(
+    document.getElementById('root');
+);
+index.render(<h1>Hi!</h1>);
--- src/script.js
+++ src/script.js
@@ -1,11 +1,15 @@
 function replacementWarning() {
-    alert("WARNING: There is a replacement for that file:\n..\\index.xml\nwhich is this HTML.\nPlease, if you want to check that XML file out again, then I'm afraid that'll be deleted and be succeeded by this HTML.\nContact:\nyoussef.land@outlook.com")
+    alert("WARNING: There is a replacement for that file:\n..\\index.xml\nwhich is this HTML.\nPlease, if you want to check that XML file out again, then I'm afraid that's been deleted and has been succeeded by this HTML.\nE-mail address:\nyoussef.land@outlook.com")
 }
-function namePrompt() {
+function linkedinNamePrompt() {
     var name = prompt("What's your name?\nPlease enter it, I'm not a hacker!")
     if (name == null || name == "") {
         alert("The name was not specified.\nSorry, but without a name, you can't enter LinkedIn via this HTML.")
     } else {
-        alert("Hello, " + name + "! I'm so glad you specified a name!")
+        alert("Hello, " + name + "! I'm so glad you specified a name! Click OK to go to LinkedIn!")
+        linkedinRedirect();
     }
+}
+function linkedinRedirect() {
+    location.href = "https://linkedin.com/feed";
 }
